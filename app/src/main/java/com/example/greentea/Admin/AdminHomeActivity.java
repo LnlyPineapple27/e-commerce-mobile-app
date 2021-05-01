dddd53d03bbfa66a7b2e51a8fceccae75a495a0f
LnlyPineapple27 <datpt1000cyber@gmail.com>
2021-05-01 20:35:02 +0700

    add Admin Home UI and its layout file

---
+++ app/src/main/java/com/example/greentea/Admin/AdminHomeActivity.java
@@ -0,0 +1,56 @@
+package com.example.greentea.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.greentea.Buyers.HomeActivity;
+import com.example.greentea.Buyers.JoinForm;
+import com.example.greentea.R;
+
+public class AdminHomeActivity extends AppCompatActivity {
+    private Button LogoutBtn, CheckOrdersBtn, maintainProductsBtn, checkApproveProductsBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_home);
+        getSupportActionBar().hide();
+        LogoutBtn = findViewById(R.id.admin_logout_btn);
+        CheckOrdersBtn = findViewById(R.id.check_orders_btn);
+        maintainProductsBtn = findViewById(R.id.maintain_btn);
+        checkApproveProductsBtn = findViewById(R.id.check_approve_products_btn);
+        maintainProductsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminHomeActivity.this, HomeActivity.class);
+                intent.putExtra("Admin", "Admin");
+                startActivity(intent);
+            }
+        });
+        LogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminHomeActivity.this, JoinForm.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+        CheckOrdersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        checkApproveProductsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}

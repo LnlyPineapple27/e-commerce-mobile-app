bc2a5197d5faf611a8ea41908f980d0ca1876254
quang <quangminhtran323@gmail.com>
2021-05-13 21:09:53 +0700

    add buyers

--- app/src/main/java/com/example/greentea/Buyers/JoinForm.java
+++ app/src/main/java/com/example/greentea/Buyers/JoinForm.java
@@ -1,4 +1,136 @@
 package com.example.greentea.Buyers;
 
-public class JoinForm {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.greentea.Model.Users;
+import com.example.greentea.Prevalent.Prevalent;
+import com.example.greentea.R;
+import com.example.greentea.Sellers.SellerHomeActivity;
+import com.example.greentea.Sellers.SellerRegistrationActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import io.paperdb.Paper;
+
+public class JoinForm extends AppCompatActivity {
+    private Button joinNowBtn, loginBtn;
+    private ProgressDialog loadingBar;
+    private TextView sellerBegin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_form);
+        getSupportActionBar().hide();
+        setStatusBarTransparent();
+        joinNowBtn = findViewById(R.id.main_join_now_btn);
+        loginBtn = findViewById(R.id.main_login_btn);
+        sellerBegin = findViewById(R.id.seller_begin);
+        loadingBar = new ProgressDialog(this);
+        Paper.init(this);
+        joinNowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(JoinForm.this, RegisterActivity.class));
+            }
+        });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(JoinForm.this, LoginActivity.class));
+            }
+        });
+        sellerBegin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(JoinForm.this, SellerRegistrationActivity.class));
+            }
+        });
+        //check data phone and password in memory and show loadingBar when user clicked remember me btn
+        String UserPhoneKey = Paper.book().read(Prevalent.UserPhoneKey);
+        String UserPasswordKey = Paper.book().read(Prevalent.UserPasswordKey);
+        if(UserPhoneKey != "" && UserPasswordKey != ""){
+            if(!TextUtils.isEmpty(UserPhoneKey) && !TextUtils.isEmpty(UserPasswordKey)){
+                AllowAccess(UserPhoneKey, UserPasswordKey);
+                loadingBar.setTitle("Already Login");
+                loadingBar.setMessage("Please wait...");
+                loadingBar.setCanceledOnTouchOutside(false);
+                loadingBar.show();
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(firebaseUser != null){
+            Intent intent = new Intent(JoinForm.this, SellerHomeActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    private void AllowAccess(final String phone, final String password) {
+        final DatabaseReference RootRef = FirebaseDatabase.getInstance().getReference();
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.child("Users").child(phone).exists()){
+                    Users usersData = snapshot.child("Users").child(phone).getValue(Users.class);
+                    if(usersData.getPhone().equals(phone)){
+                        if(usersData.getPassword().equals(password)){
+                            Toast.makeText(JoinForm.this, "Please wait, you has already logged in", Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+                            Intent intent = new Intent(JoinForm.this, HomeActivity.class);
+                            Prevalent.currentOnlineUser = usersData;
+                            startActivity(intent);
+                        }
+                        else{
+                            loadingBar.dismiss();
+                            Toast.makeText(JoinForm.this, "Password is incorrect", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+                else{
+                    Toast.makeText(JoinForm.this, "Account with phone number " + phone + " doesn't exist", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void setStatusBarTransparent(){
+        if(Build.VERSION.SDK_INT >= 21){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
 }
---
+++ app/src/main/java/com/example/greentea/Prevalent/Prevalent.java
@@ -0,0 +1,9 @@
+package com.example.greentea.Prevalent;
+
+import com.example.greentea.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}

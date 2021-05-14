83b20e725d50e29d1230fea82e351452230c59a8
LnlyPineapple27 <datpt1000cyber@gmail.com>
2021-05-14 16:43:04 +0700

    add ItemViewHolder + fix Buyer notification Bug

---
+++ app/src/main/java/com/example/greentea/ViewHolder/ItemViewHolder.java
@@ -0,0 +1,35 @@
+package com.example.greentea.ViewHolder;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.greentea.Interface.ItemClickListener;
+import com.example.greentea.R;
+
+public class ItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public TextView txtProductName, txtProductDescription, txtProductPrice, txtProductStatus;
+    public ImageView imageView;
+    public ItemClickListener listener;
+
+    public ItemViewHolder(@NonNull View itemView) {
+        super(itemView);
+        imageView = itemView.findViewById(R.id.product_seller_image);
+        txtProductName = itemView.findViewById(R.id.seller_product_name);
+        txtProductDescription = itemView.findViewById(R.id.product_seller_description);
+        txtProductPrice = itemView.findViewById(R.id.product_seller_price);
+        txtProductStatus = itemView.findViewById(R.id.seller_product_state);
+    }
+
+    public void setItemClickListener(ItemClickListener listener){
+        this.listener = listener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        listener.onClick(view, getAdapterPosition(), false);
+    }
+}
